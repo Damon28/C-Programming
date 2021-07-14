9fabdf6033e72b06728c98a425ef07c6ea49f3ac
C_Programmer <damon.maldonado.1@gmail.com>
2021-07-14 12:02:04 -0700

    Using the printf

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 
 int main()
 {
-    int age = 40;
-    double gpa = 3.7;
-    char grade = 'A';
-    char phrase[] = 'Need those brackets to create a string (AKA an array).'
-
+    printf("Almost exactly like print in Python\n");
+    printf("Use '%%d' to print out numbers. For filling containers using variables treat it like Python's .format() method.\n\n");
+    printf("EX: printf\(\"My favorite %%s is %%d\", \"number\", 500\)\n");
+    printf("Prints as: ");
+    printf("My favorite %s is %d\n\n", "number", 500);
     return 0;
 
 }
